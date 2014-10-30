e8f9e91a04b2c396879ff6fa5c9a107c71051fc8
agungperdana <agung.dodi.perdana@gmail.com>
2014-10-30 15:11:45 +0700

    add geographic

---
+++ belian/src/main/java/com/kratonsolution/belian/security/dm/Geographic.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.security.dm;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Agung Dodi Perdana
+ *
+ */
+@Getter
+@Setter
+@Document(collection="geographic")
+public class Geographic 
+{
+	public enum Type{COUNTRY,CITY,KECAMATAN,KELURAHAN,RW,RT}
+	
+	@Id
+	private String id;
+
+	@Field("code")
+	private String code;
+	
+	@Field("name")
+	private String name;
+	
+	@Field("type")
+	private Type type = Type.COUNTRY;
+	
+	public static Geographic newInstance()
+	{
+		Geographic geographic = new Geographic();
+		geographic.setId(UUID.randomUUID().toString());
+		
+		return geographic;
+	}
+}
---
+++ belian/src/main/java/com/kratonsolution/belian/security/view/GeographicController.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.security.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kratonsolution.belian.security.dm.Geographic;
+import com.kratonsolution.belian.security.dm.GeographicRepository;
+
+/**
+ * @author agungdodiperdana
+ *
+ */
+@Controller
+@RequestMapping("/geographics")
+public class GeographicController
+{
+	@Autowired
+	private GeographicRepository repository;
+		
+	@RequestMapping("/list")
+	public String list(Model model)
+	{
+		model.addAttribute("geographics",repository.findAll());		
+		return "geographics";
+	}
+	
+	@RequestMapping("/preadd")
+	public String preadd(Model model)
+	{
+		model.addAttribute("geographic",Geographic.newInstance());
+		return "geographic-add";
+	}
+	
+	@RequestMapping("/add")
+	public String add(Geographic geographic)
+	{
+		repository.save(geographic);
+		return "redirect:/geographics/list";
+	}
+	
+	@RequestMapping("/preedit/{id}")
+	public String preedit(@PathVariable String id,Model model)
+	{
+		model.addAttribute("geographic",repository.findOne(id));
+		return "geographic-edit";
+	}
+	
+	@RequestMapping("/edit")
+	public String edit(Geographic geographic)
+	{
+		repository.save(geographic);
+		return "redirect:/geographics/list";
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String delete(@PathVariable String id)
+	{
+		repository.delete(id);
+		return "redirect:/geographics/list";
+	}
+}
