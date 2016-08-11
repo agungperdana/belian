b9bd006145e78183ff4de4dc9f08d9df7fb2f9b4
ADP <agung.dodi.perdana@gmail.com>
2016-08-11 11:10:50 +0700

    add prototype for invoice module

---
+++ belian/src/main/java/com/kratonsolution/belian/invoice/dm/InvoiceAdjustment.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.invoice.dm;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com
+ */
+public class InvoiceAdjustment implements Serializable
+{
+	private BigDecimal percent;
+}
---
+++ belian/src/main/java/com/kratonsolution/belian/invoice/dm/InvoiceItemType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.invoice.dm;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com
+ */
+public enum InvoiceItemType
+{
+
+}
