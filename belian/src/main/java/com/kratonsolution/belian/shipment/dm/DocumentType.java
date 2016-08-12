c18e14ca5dc8f3459bcb864a915760cdab2e5b9d
ADP <agung.dodi.perdana@gmail.com>
2016-08-12 09:20:37 +0700

    add prototype for module shipment

---
+++ belian/src/main/java/com/kratonsolution/belian/shipment/dm/DocumentType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.shipment.dm;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com
+ */
+public enum DocumentType
+{
+
+}
---
+++ belian/src/main/java/com/kratonsolution/belian/shipment/dm/ShipmentRouteSegment.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.kratonsolution.belian.shipment.dm;
+
+import java.io.Serializable;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com
+ */
+public class ShipmentRouteSegment implements Serializable
+{
+
+}
