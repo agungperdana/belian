41e4c6d392bec42f2063918d110461a386a3558d
Agung Dodi Perdana <agungdp@localhost.localdomain>
2021-02-12 08:15:02 +0700

    add admin dashboard

---
+++ admin-dashboard/src/main/java/com/kratonsolution/belian/backoffice/Landing.java
@@ -0,0 +1,22 @@
+package com.kratonsolution.belian.backoffice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com 
+ */
+@Controller
+public class Landing {
+    
+    @GetMapping("/")
+    public String landing() {
+        return "redirect:/login";
+    }
+    
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
+}
