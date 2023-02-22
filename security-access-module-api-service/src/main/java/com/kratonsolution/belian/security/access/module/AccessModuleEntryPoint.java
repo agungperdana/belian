4a111e520a9f7a0ad2d7c45c58240ae2d64ce855
Agung Dodi Perdana <agung.dodi,.perdana@gmail.com>
2023-02-22 20:59:39 +0700

    add eureka server

--- security-access-module-api-service/src/main/java/com/kratonsolution/belian/security/access/module/AccessModuleEntryPoint.java
+++ security-access-module-api-service/src/main/java/com/kratonsolution/belian/security/access/module/AccessModuleEntryPoint.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableTransactionManagement
 @OpenAPIDefinition(info = @Info(title = "Belian ERP Access Module", version = "2.0.1", description = "API Documentation for project Access module"))
 public class AccessModuleEntryPoint {
