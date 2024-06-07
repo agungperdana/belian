23b6fd192af95c7e8f9b1e4591753fe53e0846ea
Agung Dodi Perdana <agung.dodi@unit4.com>
2024-06-07 09:26:22 +0700

    refactor s1

---
+++ common/common-app/src/main/java/com/kratonsolution/belian/common/app/SessionUtil.java
@@ -0,0 +1,87 @@
+package com.kratonsolution.belian.common.app;
+
+import com.kratonsolution.belian.common.orm.IDValueRef;
+import com.kratonsolution.belian.common.orm.Referenceable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com
+ * @since 1.0.0
+ */
+public class SessionUtil implements Referenceable {
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public SessionUtil getOrganization()
+    {
+        return this;
+    }
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public String getId()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public boolean isEmpty()
+    {
+        return true;
+    }
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public List<String> getOrganizationIds()
+    {
+        return new ArrayList();
+    }
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public String getUserName()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+    // TODO: 6/7/2024 dummy method, please fix
+    public SessionUtil getUser()
+    {
+        return this;
+    }
+
+    public SessionUtil getPerson()
+    {
+        return this;
+    }
+
+    public SessionUtil getCurrency()
+    {
+        return this;
+    }
+
+    public SessionUtil getFacility()
+    {
+        return this;
+    }
+
+    @Override
+    public String getLabel() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String getValue() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public IDValueRef toRef() {
+        return Referenceable.super.toRef();
+    }
+
+    @Override
+    public boolean isValid(IDValueRef ref) {
+        return Referenceable.super.isValid(ref);
+    }
+}
